2c18641b203227977ad43faa84ce3e64c8095722
Marek Pokorny <marekp375@gmail.com>
2017-11-08 14:08:38 -0800

    Implemented Database class for the back-end

---
+++ server/database-sqlite3.js
@@ -0,0 +1,111 @@
+// database-sqlite3.js - defines the sqlite3 implementation of the database class 
+
+var sqlite3 = require('sqlite3');    //.verbose(); 
+var database = require('./database.js');
+
+class Sqlite3Database extends database.Database {
+    constructor(filename, callback) {
+        super();
+
+        // open or create the database
+        this.db = new sqlite3.Database(filename, (err) => {
+            if (err)
+                return callback(err);
+
+            // turn on serialized mode to prevent other queries from going
+            // through before the database is ready
+            this.db.serialize();
+
+            // get all tables named "subscriptions"
+            this.db.all("SELECT name FROM sqlite_master WHERE type='table' AND name='subscriptions'",
+            (err, rows) => {
+                if (err)
+                    return callback(err);
+
+                if (rows.length == 0) {
+                    // create the subscriptions table
+                    this.db.run(`CREATE TABLE subscriptions(
+                        userId     TEXT,
+                        platform   TEXT,
+                        accountUrl TEXT,
+
+                        UNIQUE(userId, platform, accountUrl)
+                    )`);
+
+                    // create an index on userId for faster access
+                    this.db.run("CREATE INDEX idx1 ON subscriptions(userId)");
+                    this.db.run("CREATE INDEX idx2 ON subscriptions(userId, platform, accountUrl)",
+                        (err) => {
+                            // should now be safe to parallelize
+                            this.db.parallelize();
+                            callback(err);
+                        });
+                }
+            });
+        });
+    }
+
+    // helper method to figure out if a subscription exists
+    _subscriptionExists(userId, platform, subscription, callback) {
+        this.db.all("SELECT * FROM subscriptions WHERE userId=? AND platform=? and accountUrl=?",
+            [userId, platform, subscription],
+            (err, rows) => {
+                callback(err, rows ? rows.length != 0 : undefined);
+            }
+        );
+    }
+
+    addSubscription(userId, platform, subscription, callback) {
+        this._subscriptionExists(userId, platform, subscription, (err, exists) => {
+            // return error if subscription already exists
+            if (exists) {
+                return callback("Subscription already exists");
+            }
+
+            // add the subscription
+            this.db.run("INSERT INTO subscriptions(userId, platform, accountUrl) VALUES(?, ?, ?)",
+                [userId, platform, subscription],
+                (err) => {
+                    callback(err ? "Database error: " + err : null);
+                }
+            );
+        });
+    }
+
+    removeSubscription(userId, platform, subscription, callback) {
+        this._subscriptionExists(userId, platform, subscription, (err, exists) => {
+            // return error if subscription already exists
+            if (!exists) {
+                return callback("Subscription does not exist");
+            }
+
+            // remove the subscription
+            this.db.run("DELETE FROM subscriptions WHERE userId=? AND platform=? AND accountUrl=?",
+                [userId, platform, subscription],
+                (err) => {
+                    callback(err ? "Database error: " + err : null);
+                }
+            );
+        });
+    }
+
+    // TODO: platform ignored for now, still need to figure out specifics of the API
+    getSubscriptions(userId, platform, callback) {
+        this.db.all("SELECT platform, accountUrl FROM subscriptions WHERE userId=?", [userId],
+            (err, rows) => {
+                if (err)
+                    callback("Database error: " + err);
+                else
+                    callback(null, rows);
+            }
+        );
+    }
+};
+
+module.exports = {
+    // acts as a factory method
+    createDatabase: function(options, callback) {
+        return new Sqlite3Database(options.filename, callback);
+    },
+};
+
---
+++ server/database.js
@@ -0,0 +1,55 @@
+// database.js - Defines the Database "interface" class
+
+// This class acts as the "interface" for all databases. Since javascript does
+// not have interfaces, just return an error for everything
+class Database {
+    // Adds a subscription to the database
+    //
+    // Arguments:
+    //   userId - ID of the user who is subscribing
+    //   platform - the platform (e.g. youtube)
+    //   subscription - the account being subscribed to
+    //   callback(err) - called when the subscription has been added. If
+    //       successful, err is null, otherwise it's an error message string.
+    addSubscription(userId, platform, subscription, callback) {
+        throw "Not implemented";
+    }
+
+    // Removes a subscription from the database
+    //
+    // Arguments:
+    //   userId - ID of the user who is subscribing
+    //   platform - the platform (e.g. youtube)
+    //   subscription - the account being subscribed to
+    //   callback(err) - called when the subscription has been added. If
+    //       successful, err is null, otherwise it's an error message string.
+    removeSubscription(userId, platform, subscription, callback) {
+        throw "Not implemented";
+    }
+
+    // Gets all subscriptions for a particular user
+    //
+    // Arguments:
+    //   userId - ID of the user who is subscribing
+    //   platform - the platform (e.g. youtube)
+    //   callback(err, subs) - called when the subscription has been added. If
+    //       successful, err is null, and subs is:
+    //       [ { platform: "youtube", accountUrl: "http://youtube.com/channel/123blah456" }, {...} ]
+    //       otherwise, err is an error message string, and subs is null.
+    getSubscriptions(userId, platform, subscription, callback) {
+        throw "Not implemented";
+    }
+}
+
+// export the abstract Database class from this module
+module.exports = {
+    Database: Database,
+
+    // acts sort of like a factory method, but also hides the module loading
+    // callback is called on error, or when the database is ready
+    createDatabase: function(options, callback) {
+        module = require("./database-"+options.type+".js")
+        return module.createDatabase(options, callback);
+    }
+}
+
