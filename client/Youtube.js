66a05357d071a6260edcb6dd5888f7ac65b80c90
Aaron Chung <aschung212@gmail.com>
2017-11-16 00:54:59 -0800

    formatted youtube's api response and got relevant data. Embed all videos from response onto our app's page only grab latest 3 videos from a given channel instead of 25. This is just for testing and will be changed back for production.

--- client/Youtube.js
+++ client/Youtube.js
@@ -4,8 +4,38 @@
 let apiKey = "AIzaSyDQTSeTpYobZJ_dvQd_Ps_MCP90gXtjyXA";
 var platform = require('./platform.js');
 
+/**
+ * Grabs the useful information from the api response
+ * @param  {object} response - contains all of the data received from the Youtube server
+ */
 function formatResponse(response) {
+    var contentList = [];
     console.log(response);
+    for(var i in response.items) {
+        var item = response.items[i];
+        let content = {
+            'videoId': item.id.videoId, 
+            'timestamp': item.snippet.publishedAt, 
+            'platform': "youtube"
+        };
+        contentList.push(content);
+    }
+    // console.log(output);
+    embed(contentList);
+}
+
+/**
+ * Places all content in the contentList at the bottom of the webpage
+ * @param  {Array} contentList - A list of the content to be embeded
+ */
+function embed(contentList) {
+    for (var i in contentList) {
+        let iframe = document.createElement('iframe');
+        iframe.src = "https://www.youtube.com/embed/" + contentList[i].videoId;
+        iframe.width = "560";
+        iframe.height = "315";
+        document.body.appendChild(iframe);
+    }
 }
 
 /**
@@ -33,13 +63,17 @@ class Youtube extends platform.Platform {
         xhr.send();
     }
 
-    /**
-     * This method embeds a given url to the application at the bottom of the page.
-     * @param  {String} contentUrl - the content url to be embedded
-     */
-    embed(contentUrl) {
-    
-    }
+    // /**
+    //  * This method embeds a given url to the application at the bottom of the page.
+    //  * @param  {String} contentUrl - the content url to be embedded
+    //  */
+    // embed(contentUrl) {
+    //     let iframe = document.createElement('iframe');
+    //     iframe.src = "https://www.youtube.com/embed/" + contentUrl[0].videoId;
+    //     iframe.width = "560";
+    //     iframe.height = "315";
+    //     document.body.appendChild(iframe);
+    // }
 
 }
 
