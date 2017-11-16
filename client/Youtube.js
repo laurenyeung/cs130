b664933b49db95ac929d4571f3e8731392f3bf75
Aaron Chung <aschung212@gmail.com>
2017-11-16 00:54:59 -0800

    formatted youtube's api response and got relevant data. Embed all videos from response onto our app's page

--- client/Youtube.js
+++ client/Youtube.js
@@ -4,8 +4,52 @@
 let apiKey = "AIzaSyDQTSeTpYobZJ_dvQd_Ps_MCP90gXtjyXA";
 var platform = require('./platform.js');
 
+/**
+ * Grabs the useful information from the api response
+ * @param  {object} response - contains all of the data received from the Youtube server
+ */
 function formatResponse(response) {
+    var contentList = [];
     console.log(response);
+    for(var i in response.items) {
+        var item = response.items[i];
+        let content = {
+            'videoId': item.id.videoId, 
+            'timestamp': item.snippet.publishedAt,
+            'description': item.snippet.description,
+            'title': item.snippet.title,
+            'platform': "youtube"
+        };
+        contentList.push(content);
+    }
+    // console.log(output);
+    embed(contentList);
+}
+
+/**
+ * Places all content in the contentList at the bottom of the webpage
+ * @param  {Array} contentList - A list of the content to be embeded
+ */
+function embed(contentList) {
+    for (var i in contentList) {
+        let div = document.createElement('div');
+        let iframe = document.createElement('iframe');
+        iframe.src = "https://www.youtube.com/embed/" + contentList[i].videoId;
+        iframe.width = "560";
+        iframe.height = "315";
+        iframe.style = "text-align:center";
+        iframe.setAttribute('allowFullScreen', '');
+
+        let h3 = document.createElement('h3');
+        h3.innerHTML = contentList[i].title;
+
+        let ul2 = document.createElement('ul');
+        ul2.innerHTML = contentList[i].description;
+        div.appendChild(h3);
+        div.appendChild(ul2);
+        div.appendChild(iframe);
+        document.body.appendChild(div);
+    }
 }
 
 /**
@@ -33,13 +77,17 @@ class Youtube extends platform.Platform {
         xhr.send();
     }
 
-    /**
-     * This method embeds a given url to the application at the bottom of the page.
-     * @param  {String} contentUrl - the content url to be embedded
-     */
-    embed(contentUrl) {
-    
-    }
+    // /**
+    //  * This method embeds a given url to the application at the bottom of the page.
+    //  * @param  {String} contentUrl - the content url to be embedded
+    //  */
+    // embed(contentUrl) {
+    //     let iframe = document.createElement('iframe');
+    //     iframe.src = "https://www.youtube.com/embed/" + contentUrl[0].videoId;
+    //     iframe.width = "560";
+    //     iframe.height = "315";
+    //     document.body.appendChild(iframe);
+    // }
 
 }
 
