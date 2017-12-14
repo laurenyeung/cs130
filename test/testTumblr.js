162b702fa03bdfab270da366ddeb73946db06a62
JoannePark <jpark.per3@gmail.com>
2017-12-14 15:31:48 -0800

    test file for Tumblr

---
+++ test/testTumblr.js
@@ -0,0 +1,19 @@
+// testTumblr.js - Unit test for the Tumblr class
+var assert = require("assert");
+var TumblrModule = require("../client/Tumblr.js");
+
+describe('Tumblr', function() {
+
+    describe('#getPlaceholder()', function() {
+        });
+
+    describe('#getContent()', function() {
+        });
+
+    describe('#embed()', function() {
+        });
+
+    describe('#getAccountUrl()', function() {
+        });
+
+});
